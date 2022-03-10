666eef939259f400844fd0ef2aa7135f354e552b
yukyu30 <fukanoyugo+dev@gmail.com>
2022-03-10 21:58:27 +0900

    1 file modified, 1 added, 2 deleted

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+const ProfileImage = () => (
+  <Image src="/images/profile.jpg" height={144} width={144} alt="yukyu" />
+);
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post </h1>
+      <ProfileImage />
       <h2>
         <Link href="/">
           <a>Back to home </a>
